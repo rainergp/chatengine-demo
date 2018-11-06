6e838673fb434fe7e065f12fb9f5bad685439582
Rainer Gonzalez <rainer.gonzalez@levatas.com>
2018-11-06 18:57:15 -0500

    users list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+
 import { ChatEngine } from './chat-engine.service';
+
+import {AppComponent} from './app.component';
 import { AppUsersOnlineComponent } from './app.users-online.component';
 
 @NgModule({
